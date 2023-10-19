1c67299a18cd97a00ca25c7881975e8200a025a5
karuirujackson <jkjackaruiru641@gmail.com>
2023-10-19 17:27:11 +0300

    Added a .removeTail() method.

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,11 +1,15 @@
+//Import Node 
 const Node = require('./Node');
 
+////////////////////////////////////////////////////////////////////////
+//Create a class for the doubly linked list with a constructor and methods
 class DoublyLinkedList {
   constructor () {
     this.head = null;
     this.tail = null;
   }
-
+   ///////////////////////////////////////////////////////////
+   //An add a node to Head method
   addToHead (data) {
     const newHead = new Node(data);
     const currentHead = this.head;
@@ -18,7 +22,8 @@ class DoublyLinkedList {
         this.tail = newHead;
     }
   }
-
+  ///////////////////////////////////////////////////////////
+   //An add a node to tail method
   addToTail (data) {
     const newTail = new Node(data);
     const currentTail = this.tail;
@@ -31,11 +36,12 @@ class DoublyLinkedList {
         this.head = newTail;
     }
   }
-
+  ///////////////////////////////////////////////////////////
+   //A remove head method
   removeHead() {
     const removedHead = this.head;
     if(removedHead === null) {
-        return;
+        return; //Check if removedTail has value. If not, that means there’s nothing to remove, so return to end the method.
     }
     this.head = removedHead.getNextNode();
     if(this.head) {
@@ -46,7 +52,23 @@ class DoublyLinkedList {
     }
     return removedHead.data;
   }
-  
+  ///////////////////////////////////////////////////////////
+ //A remove tail method
+  removeTail() {
+    const removedTail = this.tail;
+    if(removedTail === null) {
+        return;
+    }
+    this.tail = removedTail.getPreviousNode();
+    if(this.tail) {
+        this.tail = setNextNode(null);
+    }
+    if(removedTail === this.head) {
+        this.removeHead();
+    }
+    return removedTail.data;
+  }
+
   printList() {
     let currentNode = this.head;
     let output = '<head> ';
